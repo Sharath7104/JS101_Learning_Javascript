f49075e832b884f831d29ea9fba6214da238ec31
Sharath7104 <sharathkutty143@gmail.com>
2022-09-03 19:18:20 +0530

    Adding Mathematical_Operater from second location

---
+++ MathematicalOperators/problem3.js
@@ -0,0 +1,17 @@
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+var five = 5;
+var six = 6;
+
+var two = (two * 2);
+var five = (five * 2);
+var six = (six * 2);
+var three = (three * 3);
+var four = (four * 3);
+
+
+
+console.log(one + two + three + four + five + six);
+
